f9e8506c97c97d6d2ffadb51e0b2c9c0ea7ea8ea
sudip <sudipshil9862@gmail.com>
2022-08-21 10:54:32 +0530

    first program's name changed

---
+++ 0-firstWindow-oneButton.py
@@ -0,0 +1,18 @@
+import gi  #gi. repository is the python module for PyGObject
+gi.require_version("Gtk", "4.0") 
+from gi.repository import Gtk
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    win.set_title('first application in python in gtk4')
+    win.set_default_size(350,350);
+
+    btn = Gtk.Button(label="Hello, World!")
+    btn.connect('clicked', lambda x: win.close())
+    win.set_child(btn)
+
+    win.present()
+
+app = Gtk.Application(application_id='org.gtk.Example')
+app.connect('activate', on_activate)
+app.run(None)
